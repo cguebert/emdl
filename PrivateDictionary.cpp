9a98b86dd2f60f246addec9e12cc4349951b0e66
Christophe Guébert <c.guebert@e-media.fr>
2017-09-27 08:48:52 +0000

    Add a private tags dictionary.

---
+++ PrivateDictionary.cpp
@@ -0,0 +1,40 @@
+#include <emdl/PrivateDictionary.h>
+
+#include <map>
+
+namespace
+{
+	using PrivateTagKey = std::tuple<std::uint16_t, std::string, std::uint8_t>;
+	using PrivateTagsMap = std::map<PrivateTagKey, const emdl::PrivateDictionaryEntry*>;
+
+	const PrivateTagsMap& getPrivateTagsMap()
+	{
+		static const PrivateTagsMap tagsMap = [] {
+			PrivateTagsMap tagsMap;
+			const auto tagsList = emdl::registry::getPrivateDictionary();
+			for (const auto& entry : tagsList)
+				tagsMap[PrivateTagKey{entry.group, entry.creator, entry.element}] = &entry;
+			return tagsMap;
+		}();
+
+		return tagsMap;
+	}
+}
+
+namespace emdl
+{
+	namespace registry
+	{
+		const PrivateDictionaryEntry* getPrivateTag(Tag tag, const std::string& creator)
+		{
+			std::uint8_t element = tag.element & 0xFF;
+			PrivateTagKey key{tag.group, creator, element};
+			const auto& tagsMap = getPrivateTagsMap();
+			const auto it = tagsMap.find(key);
+			if (it != tagsMap.end())
+				return it->second;
+
+			return nullptr;
+		}
+	}
+}
---
+++ PrivateDictionary.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <emdl/Tag.h>
+
+#include <vector>
+
+namespace emdl
+{
+	/// Entry in a dictionary of private DICOM tags
+	struct EMDL_API PrivateDictionaryEntry
+	{
+		PrivateDictionaryEntry(std::uint16_t group,
+							   const char* creator,
+							   std::uint8_t element,
+							   const char* keyword,
+							   const char* vr,
+							   const char* vm,
+							   const char* code)
+			: group(group)
+			, creator(creator)
+			, element(element)
+			, keyword(keyword)
+			, vr(vr)
+			, vm(vm)
+			, code(code)
+		{
+		}
+
+		std::uint16_t group = 0; /// Group containing this private tag.
+		const char* creator; /// Creator of this private tag.
+		std::uint8_t element = 0; /// Last 8 bits of the element part of the tag.
+		const char* keyword; /// Brief name of the tag.
+		const char* vr; /// Type.
+		const char* vm; /// Multiplicity.
+		const char* code; /// Code for the de-identification action.
+	};
+
+	using PrivateTags = std::vector<PrivateDictionaryEntry>;
+
+	namespace registry
+	{
+		EMDL_API const PrivateTags& getPrivateDictionary();
+		EMDL_API const PrivateDictionaryEntry* getPrivateTag(Tag tag, const std::string& creator);
+	}
+}
